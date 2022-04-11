2e5925b17b0a0eccda9277069295e61d1c0ae3b5
SmartTamalonggehe <smartspartacus@gmail.com>
2022-04-12 03:37:16 +0900

    menambahkan visi misi

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,7 +1,15 @@
 @extends('admin.layouts.default')
-@section('title', 'Halaman Dashboard')
+@section('title', '')
 @section('content')
     <div class="col-12">
-        <h3 class="text-center">SELAMAT DATANG DI WEBSITE</h3>
+        <h3 class="text-center text-uppercase">SELAMAT DATANG DI WEBSITE SPT DAN SPPD <br> dinas olahraga dan pemuda provinsi
+            papua
+        </h3>
+        <img src="{{ asset('images/gedung.jpg') }}" class="img-fluid mx-auto d-block my-3 shadow-lg bg-body rounded"
+            alt="...">
+        <h4>Visi</h4>
+        <p class="fs-5">Mewujudkan kebangkitan keolahragaan dan kepemudaan</p>
+        <h4>Misi</h4>
+        <p class="fs-5">Meningkatkan daya saing keolahragaan dan kepemudaan</p>
     </div>
 @endsection
